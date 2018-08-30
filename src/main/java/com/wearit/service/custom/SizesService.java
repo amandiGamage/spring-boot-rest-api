cafe5734bffbd7641d658f754b4c7412ed35c39c
Amandi Imasha <Amandiimasha43@gmail.com>
2018-08-31 02:17:25 +0530

    extends custom service via super service

---
+++ src/main/java/com/wearit/service/custom/SizesService.java
@@ -0,0 +1,10 @@
+package com.wearit.service.custom;
+
+import com.wearit.dto.SizesDTO;
+import com.wearit.service.SuperService;
+
+/**
+ * Created by ABC on 8/31/2018.
+ */
+public interface SizesService extends SuperService<SizesDTO> {
+}
