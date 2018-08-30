b7c1608ae41eb5c8458a349f02110bbf1400bd2d
Amandi Imasha <Amandiimasha43@gmail.com>
2018-08-31 03:16:29 +0530

    add dependency sprin hateos

---
+++ src/main/java/com/wearit/controller/SizesController.java
@@ -0,0 +1,10 @@
+package com.wearit.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Amandi on 8/31/2018.
+ */
+@RestController
+public class SizesController {
+}
