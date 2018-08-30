8b31cd00a1205da913c79f7fb203996dab16e74b
Amandi Imasha <Amandiimasha43@gmail.com>
2018-08-31 01:04:39 +0530

    add custom interface for de cupling

---
+++ src/main/java/com/wearit/dao/custom/SizeDao.java
@@ -0,0 +1,10 @@
+package com.wearit.dao.custom;
+
+import com.wearit.dao.SuperDao;
+import com.wearit.model.Sizes;
+
+/**
+ * Created by Amandi on 8/31/2018.
+ */
+public interface SizeDao extends SuperDao<Sizes>{
+}
