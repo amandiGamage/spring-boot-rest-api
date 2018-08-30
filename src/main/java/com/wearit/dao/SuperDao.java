c589d61f99835ba177716226db5d46a1317958be
Amandi Imasha <Amandiimasha43@gmail.com>
2018-08-31 01:02:56 +0530

    write super DAO interface for decupling

--- src/main/java/com/wearit/dao/SuperDao.java
+++ src/main/java/com/wearit/dao/SuperDao.java
@@ -1,7 +1,14 @@
 package com.wearit.dao;
 
+import java.util.List;
+
 /**
  * Created by ABC on 8/31/2018.
  */
-public interface SuperDao {
+public interface SuperDao <T> {
+    public  T add(T t)throws Exception;
+    public boolean update(T t)throws Exception;
+    public int delete(Integer id)throws Exception;
+    public T search(Integer id)throws Exception;
+    public List<T> getAll(Integer offset,Integer limit)throws Exception;
 }
