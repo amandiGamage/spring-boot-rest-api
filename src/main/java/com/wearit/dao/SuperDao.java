db84ea1b4920ab021c1ebed1be72d0f5e2f0119a
Amandi Imasha <Amandiimasha43@gmail.com>
2018-08-31 01:10:20 +0530

    change return type

--- src/main/java/com/wearit/dao/SuperDao.java
+++ src/main/java/com/wearit/dao/SuperDao.java
@@ -8,7 +8,7 @@ import java.util.List;
 public interface SuperDao <T> {
     public  T add(T t)throws Exception;
     public boolean update(T t)throws Exception;
-    public int delete(Integer id)throws Exception;
+    public boolean delete(Integer id)throws Exception;
     public T search(Integer id)throws Exception;
     public List<T> getAll(Integer offset,Integer limit)throws Exception;
 }
