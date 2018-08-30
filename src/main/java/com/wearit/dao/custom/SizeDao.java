65169a570ab03976dc220a5ccdf6a27555d34009
Amandi Imasha <Amandiimasha43@gmail.com>
2018-08-31 01:12:11 +0530

    add method for size searching by name

--- src/main/java/com/wearit/dao/custom/SizeDao.java
+++ src/main/java/com/wearit/dao/custom/SizeDao.java
@@ -7,4 +7,5 @@ import com.wearit.model.Sizes;
  * Created by Amandi on 8/31/2018.
  */
 public interface SizeDao extends SuperDao<Sizes>{
+    public Sizes getSizeByName(String size) throws Exception;
 }
