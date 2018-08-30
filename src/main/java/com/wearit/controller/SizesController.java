f256328fed3a2175e6f499cf428101873cc7e8e1
Amandi Imasha <Amandiimasha43@gmail.com>
2018-08-31 04:07:43 +0530

    add RestController Size Api Endpoint

--- src/main/java/com/wearit/controller/SizesController.java
+++ src/main/java/com/wearit/controller/SizesController.java
@@ -1,10 +1,11 @@
 package com.wearit.controller;
 
+
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Amandi on 8/31/2018.
- */
+ * */
 @RestController
 public class SizesController {
 }
